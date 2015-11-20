3f86cb64be60d29b37dc71de130c2b731e489e3f
nyradr <nyradr@protonmail.com>
2015-11-20 12:01:02 +0100

    Commentaires Deck

--- src/Deck.java
+++ src/Deck.java
@@ -3,23 +3,48 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Stack;
 
-
+/**	Représente un deck de cartes
+ * 	Un deck est une pile de carte
+ */
 public class Deck {
 	
-	ArrayList<Carte> deck;
+	ArrayList<Carte> deck;	//pile de cartes, le haut de la pile est la fin de la liste
 	
-	int nmax;
+	int nmax;				//nombre maximal d'elements
 	
+	/**	Constructeur
+	 * 	@param n : nombre max de cartes
+	 */
 	public Deck(int n){
 		this.deck = new ArrayList<Carte>(n);
 		this.nmax = n;
 	}
 	
+	/**	Indique si le deck est vide
+	 * 	@return true si le deck est vide
+	 */
+	public boolean isEmpty(){
+		return this.deck.size() == 0;
+	}
+	
+	/**	Indique si le deck est plein
+	 * 	Un deck est plein quand le nombre de carte qu'il contient est egal au nombre max de cartes authoriser
+	 * 	@return	true si le deck est plein
+	 */
+	public boolean isFull(){
+		return ! (this.deck.size() < this.nmax);
+	}
+	
+	/**	Ajoute une carte au deck
+	 * 	@param c	: carte à ajouter
+	 */
 	public void addCard(Carte c){
 		if(this.deck.size() < this.nmax)
 			this.deck.add(c);
 	}
 	
+	/**	Mélange le deck alèatoirement
+	 */
 	public void generateAleat(){
 		ArrayList<Carte> newdeck = new ArrayList<Carte>(this.nmax);
 		Random rand = new Random();
@@ -34,4 +59,14 @@ public class Deck {
 		this.deck = newdeck;
 	}
 	
+	/**	Pioche la carte situer au sommet du deck 
+	 * 	@return carte piocher ou null si le deck est vide
+	 */
+	public Carte pioche(){
+		int it = this.deck.size() -1;
+		Carte c = this.deck.get(it);
+		this.deck.remove(it);
+		
+		return c;
+	}
 }
