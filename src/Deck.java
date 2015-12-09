e5319fd6da8153b2dd0950c256ed375eb9aef1d8
chatmans <chatmans@free.fr>
2015-12-10 00:07:35 +0100

    Verification d'occurence de toutes les cartes dans le deck
    valableDeck()

--- src/Deck.java
+++ src/Deck.java
@@ -138,4 +138,15 @@ public class Deck {
 
 		return occ;
 	}
+	
+	
+	public boolean valableDeck(Deck deck){
+		boolean test = true;
+		for(int i = 0; i < deck.size() || test; i++){
+			if( occurInDeck(this.deck.get(i)) > this.deck.get(i).getMax())
+				test = false;
+		}
+		return test;
+	}
+	
 }
